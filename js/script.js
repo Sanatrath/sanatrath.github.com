89f7c5cd1c9cfe239f50e47d8c0e18349c932848
Sanatrath <mail@sanatrath.com>
2013-02-17 02:55:05 -0500

    color changes and other edits

--- js/script.js
+++ js/script.js
@@ -7,10 +7,8 @@ $(window).load(function() {
 		nextText: "",               
 		keyboard: true,                
 		animationLoop: false,
+		mousewheel:true,
 		});
-		
-	
-
  
 $('article').hover(
       function() {
@@ -20,4 +18,15 @@ $('article').hover(
         $(this).removeClass('focused');
       }
     );
+});
+
+$('a[href^="#"]').bind('click.smoothscroll',function (e) {
+    e.preventDefault();
+    var target = this.hash;
+        $target = $(target);
+    $('html, body').stop().animate({
+        'scrollTop': $target.offset().top
+    }, 1000, function () {
+        window.location.hash = target;
+    });
 });
